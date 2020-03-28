e5dbc0df1a4ced6691b98ac6fc488845bf5bc974
Henrique <henrique.nogueira@rede.ulbra.br>
2020-03-28 18:43:01 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LojaVirtualCarrinho';
+
+  produtos = [
+    {nome: "Produto 1",
+  preco: 100,
+  quantidade: 1},
+  {nome: "Produto 2",
+  preco: 150,
+  quantidade: 1},
+  ];
+
+  carrinho = [];
+
+  soma=0;
+
+  comprar(produto){
+    if (this.carrinho.indexOf(produto) === -1){
+      this.carrinho.push(produto);
+    }
+
+    else{
+      produto.quantidade+=1
+    }
+    this.soma+=produto.preco;
+  }
 }
